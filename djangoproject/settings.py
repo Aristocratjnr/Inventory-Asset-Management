f725fccb7393f8cef50350ee65d2217f6e5d6cab
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-15 14:14:46 +0000

    chore: Update added Dotenv to the requirement.txt

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h5=^p5^@#3wcu^$th1116e7&_ykk8c@#gye0iik!c(0ktr4rui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
