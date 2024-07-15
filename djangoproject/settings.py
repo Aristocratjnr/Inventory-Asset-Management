65bfe4eb81a59add2e4acbd41969c97a9dd0b803
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-15 15:02:29 +0000

    Update DEBUG setting to True in settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h5=^p5^@#3wcu^$th1116e7&_ykk8c@#gye0iik!c(0ktr4rui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
