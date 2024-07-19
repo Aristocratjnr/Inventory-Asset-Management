0ff44c0cbe2038eca96e9fb30d02e8c64c6c3453
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-19 17:53:12 +0000

    Update list_display in ComputerAdmin to include MAC_address field

--- djangoapp/admin.py
+++ djangoapp/admin.py
@@ -6,7 +6,7 @@ from .forms import ComputerForm
 
 
 class ComputerAdmin(admin.ModelAdmin):
-    list_display = ["computer_name", "IP_address", "Warranty", "users_name", "purchase_date","timestamp"]
+    list_display = ["computer_name", "IP_address", "MAC_address", "users_name", "purchase_date","timestamp"]
     form = ComputerForm
     list_filter = ['computer_name', 'IP_address']
     search_fields = ['computer_name', 'IP_address']
